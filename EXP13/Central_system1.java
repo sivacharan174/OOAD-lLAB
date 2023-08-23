210262570964790ebd67734f3103030e4e063664
sivacharan174 <129138943+sivacharan174@users.noreply.github.com>
2023-08-23 08:53:28 +0530

    Add files via upload

---
+++ EXP13/Central_system1.java
@@ -0,0 +1,86 @@
+
+
+/**
+ * Class Central_system1
+ */
+public class Central_system1 {
+
+  //
+  // Fields
+  //
+
+  private void store;
+  private void update;
+  
+  //
+  // Constructors
+  //
+  public Central_system1 () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of store
+   * @param newVar the new value of store
+   */
+  private void setStore (void newVar) {
+    store = newVar;
+  }
+
+  /**
+   * Get the value of store
+   * @return the value of store
+   */
+  private void getStore () {
+    return store;
+  }
+
+  /**
+   * Set the value of update
+   * @param newVar the new value of update
+   */
+  private void setUpdate (void newVar) {
+    update = newVar;
+  }
+
+  /**
+   * Get the value of update
+   * @return the value of update
+   */
+  private void getUpdate () {
+    return update;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void storing()
+  {
+  }
+
+
+  /**
+   */
+  public void updating()
+  {
+  }
+
+
+  /**
+   */
+  public void processing()
+  {
+  }
+
+
+}
---
+++ EXP13/Customer1.java
@@ -0,0 +1,86 @@
+
+
+/**
+ * Class Customer1
+ */
+public class Customer1 {
+
+  //
+  // Fields
+  //
+
+  private void name;
+  private void purchase_product;
+  
+  //
+  // Constructors
+  //
+  public Customer1 () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of name
+   * @param newVar the new value of name
+   */
+  private void setName (void newVar) {
+    name = newVar;
+  }
+
+  /**
+   * Get the value of name
+   * @return the value of name
+   */
+  private void getName () {
+    return name;
+  }
+
+  /**
+   * Set the value of purchase_product
+   * @param newVar the new value of purchase_product
+   */
+  private void setPurchase_product (void newVar) {
+    purchase_product = newVar;
+  }
+
+  /**
+   * Get the value of purchase_product
+   * @return the value of purchase_product
+   */
+  private void getPurchase_product () {
+    return purchase_product;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void payment()
+  {
+  }
+
+
+  /**
+   */
+  public void feedback()
+  {
+  }
+
+
+  /**
+   */
+  public void customer()
+  {
+  }
+
+
+}
