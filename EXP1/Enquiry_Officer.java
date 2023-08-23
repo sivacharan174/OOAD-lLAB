4ea2e8e66b6712f3bf89697c965f48aa7cb60af9
sivacharan174 <129138943+sivacharan174@users.noreply.github.com>
2023-08-23 08:08:19 +0530

    Add files via upload

---
+++ EXP1/Enquiry_Officer.java
@@ -0,0 +1,62 @@
+
+
+/**
+ * Class Enquiry_Officer
+ */
+public class Enquiry_Officer {
+
+  //
+  // Fields
+  //
+
+  private void verification_of_details;
+  
+  //
+  // Constructors
+  //
+  public Enquiry_Officer () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of verification_of_details
+   * @param newVar the new value of verification_of_details
+   */
+  private void setVerification_of_details (void newVar) {
+    verification_of_details = newVar;
+  }
+
+  /**
+   * Get the value of verification_of_details
+   * @return the value of verification_of_details
+   */
+  private void getVerification_of_details () {
+    return verification_of_details;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void issue_of_passport()
+  {
+  }
+
+
+  /**
+   */
+  public void renewal_of_passport()
+  {
+  }
+
+
+}
---
+++ EXP1/Passport_Management_System.java
@@ -0,0 +1,86 @@
+
+
+/**
+ * Class Passport_Management_System
+ */
+public class Passport_Management_System {
+
+  //
+  // Fields
+  //
+
+  private - verify_details;
+  private void store_proof;
+  
+  //
+  // Constructors
+  //
+  public Passport_Management_System () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of verify_details
+   * @param newVar the new value of verify_details
+   */
+  private void setVerify_details (- newVar) {
+    verify_details = newVar;
+  }
+
+  /**
+   * Get the value of verify_details
+   * @return the value of verify_details
+   */
+  private - getVerify_details () {
+    return verify_details;
+  }
+
+  /**
+   * Set the value of store_proof
+   * @param newVar the new value of store_proof
+   */
+  private void setStore_proof (void newVar) {
+    store_proof = newVar;
+  }
+
+  /**
+   * Get the value of store_proof
+   * @return the value of store_proof
+   */
+  private void getStore_proof () {
+    return store_proof;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void issue_of_passport()
+  {
+  }
+
+
+  /**
+   */
+  public void verification_of_passport()
+  {
+  }
+
+
+  /**
+   */
+  public void cancellation_of_passport()
+  {
+  }
+
+
+}
