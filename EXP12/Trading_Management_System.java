33496fd2765ac1dc28a6a6471526696aa662e965
sivacharan174 <129138943+sivacharan174@users.noreply.github.com>
2023-08-23 08:47:10 +0530

    Add files via upload

---
+++ EXP12/Trading_Management_System.java
@@ -0,0 +1,86 @@
+
+
+/**
+ * Class Trading_Management_System
+ */
+public class Trading_Management_System {
+
+  //
+  // Fields
+  //
+
+  private void verify_product;
+  private void amount;
+  
+  //
+  // Constructors
+  //
+  public Trading_Management_System () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of verify_product
+   * @param newVar the new value of verify_product
+   */
+  private void setVerify_product (void newVar) {
+    verify_product = newVar;
+  }
+
+  /**
+   * Get the value of verify_product
+   * @return the value of verify_product
+   */
+  private void getVerify_product () {
+    return verify_product;
+  }
+
+  /**
+   * Set the value of amount
+   * @param newVar the new value of amount
+   */
+  private void setAmount (void newVar) {
+    amount = newVar;
+  }
+
+  /**
+   * Get the value of amount
+   * @return the value of amount
+   */
+  private void getAmount () {
+    return amount;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void transport()
+  {
+  }
+
+
+  /**
+   */
+  public void delivery_product()
+  {
+  }
+
+
+  /**
+   */
+  public void money_transfer()
+  {
+  }
+
+
+}
