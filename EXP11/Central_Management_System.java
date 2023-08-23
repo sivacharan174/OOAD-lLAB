7bd7a46589987e4cb03402a083fb6cd45f431b67
sivacharan174 <129138943+sivacharan174@users.noreply.github.com>
2023-08-23 08:44:26 +0530

    Add files via upload

---
+++ EXP11/Central_Management_System.java
@@ -0,0 +1,103 @@
+
+
+/**
+ * Class Central_Management_System
+ */
+public class Central_Management_System {
+
+  //
+  // Fields
+  //
+
+  private void Member_details;
+  private void Function_details;
+  private void Details_of_seat_allocation;
+  
+  //
+  // Constructors
+  //
+  public Central_Management_System () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of Member_details
+   * @param newVar the new value of Member_details
+   */
+  private void setMember_details (void newVar) {
+    Member_details = newVar;
+  }
+
+  /**
+   * Get the value of Member_details
+   * @return the value of Member_details
+   */
+  private void getMember_details () {
+    return Member_details;
+  }
+
+  /**
+   * Set the value of Function_details
+   * @param newVar the new value of Function_details
+   */
+  private void setFunction_details (void newVar) {
+    Function_details = newVar;
+  }
+
+  /**
+   * Get the value of Function_details
+   * @return the value of Function_details
+   */
+  private void getFunction_details () {
+    return Function_details;
+  }
+
+  /**
+   * Set the value of Details_of_seat_allocation
+   * @param newVar the new value of Details_of_seat_allocation
+   */
+  private void setDetails_of_seat_allocation (void newVar) {
+    Details_of_seat_allocation = newVar;
+  }
+
+  /**
+   * Get the value of Details_of_seat_allocation
+   * @return the value of Details_of_seat_allocation
+   */
+  private void getDetails_of_seat_allocation () {
+    return Details_of_seat_allocation;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void Storing()
+  {
+  }
+
+
+  /**
+   */
+  public void Updating_details()
+  {
+  }
+
+
+  /**
+   */
+  public void Central_Management_System()
+  {
+  }
+
+
+}
---
+++ EXP11/Organizer.java
@@ -0,0 +1,86 @@
+
+
+/**
+ * Class Organizer
+ */
+public class Organizer {
+
+  //
+  // Fields
+  //
+
+  private void Member_details;
+  private void Function_details;
+  
+  //
+  // Constructors
+  //
+  public Organizer () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of Member_details
+   * @param newVar the new value of Member_details
+   */
+  private void setMember_details (void newVar) {
+    Member_details = newVar;
+  }
+
+  /**
+   * Get the value of Member_details
+   * @return the value of Member_details
+   */
+  private void getMember_details () {
+    return Member_details;
+  }
+
+  /**
+   * Set the value of Function_details
+   * @param newVar the new value of Function_details
+   */
+  private void setFunction_details (void newVar) {
+    Function_details = newVar;
+  }
+
+  /**
+   * Get the value of Function_details
+   * @return the value of Function_details
+   */
+  private void getFunction_details () {
+    return Function_details;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void Allocating_the_seats()
+  {
+  }
+
+
+  /**
+   */
+  public void Inviting_the_delegates()
+  {
+  }
+
+
+  /**
+   */
+  public void Choosing_the_winner()
+  {
+  }
+
+
+}
