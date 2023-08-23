4b9fdb030fe4c1193533da3bb001fc97bf265770
sivacharan174 <129138943+sivacharan174@users.noreply.github.com>
2023-08-23 09:36:53 +0530

    Add files via upload

---
+++ EXP24/product.java
@@ -0,0 +1,89 @@
+
+
+/**
+ * Class product
+ */
+public class product {
+
+  //
+  // Fields
+  //
+
+  private void id_name;
+  private void price;
+  private void quantity;
+  
+  //
+  // Constructors
+  //
+  public product () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of id_name
+   * @param newVar the new value of id_name
+   */
+  private void setId_name (void newVar) {
+    id_name = newVar;
+  }
+
+  /**
+   * Get the value of id_name
+   * @return the value of id_name
+   */
+  private void getId_name () {
+    return id_name;
+  }
+
+  /**
+   * Set the value of price
+   * @param newVar the new value of price
+   */
+  private void setPrice (void newVar) {
+    price = newVar;
+  }
+
+  /**
+   * Get the value of price
+   * @return the value of price
+   */
+  private void getPrice () {
+    return price;
+  }
+
+  /**
+   * Set the value of quantity
+   * @param newVar the new value of quantity
+   */
+  private void setQuantity (void newVar) {
+    quantity = newVar;
+  }
+
+  /**
+   * Get the value of quantity
+   * @return the value of quantity
+   */
+  private void getQuantity () {
+    return quantity;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void expirydate()
+  {
+  }
+
+
+}
