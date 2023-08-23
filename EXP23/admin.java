18e76582ee506ec081b7c3717ee6b2f3d0bbb339
sivacharan174 <129138943+sivacharan174@users.noreply.github.com>
2023-08-23 09:34:50 +0530

    Add files via upload

---
+++ EXP23/admin.java
@@ -0,0 +1,96 @@
+
+
+/**
+ * Class admin
+ */
+public class admin {
+
+  //
+  // Fields
+  //
+
+  private void admin_id;
+  private void name;
+  private void email_id;
+  
+  //
+  // Constructors
+  //
+  public admin () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of admin_id
+   * @param newVar the new value of admin_id
+   */
+  private void setAdmin_id (void newVar) {
+    admin_id = newVar;
+  }
+
+  /**
+   * Get the value of admin_id
+   * @return the value of admin_id
+   */
+  private void getAdmin_id () {
+    return admin_id;
+  }
+
+  /**
+   * Set the value of name
+   * @param newVar the new value of name
+   */
+  private void setName (void newVar) {
+    name = newVar;
+  }
+
+  /**
+   * Get the value of name
+   * @return the value of name
+   */
+  private void getName () {
+    return name;
+  }
+
+  /**
+   * Set the value of email_id
+   * @param newVar the new value of email_id
+   */
+  private void setEmail_id (void newVar) {
+    email_id = newVar;
+  }
+
+  /**
+   * Get the value of email_id
+   * @return the value of email_id
+   */
+  private void getEmail_id () {
+    return email_id;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void manager_product()
+  {
+  }
+
+
+  /**
+   */
+  public void view_logs()
+  {
+  }
+
+
+}
