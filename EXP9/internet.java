ddb957e5fbb13a70897529eed3938584ffb240b0
sivacharan174 <129138943+sivacharan174@users.noreply.github.com>
2023-08-23 08:37:19 +0530

    Add files via upload

---
+++ EXP9/internet.java
@@ -0,0 +1,113 @@
+
+
+/**
+ * Class internet
+ */
+public class internet {
+
+  //
+  // Fields
+  //
+
+  private void enter_id;
+  private void download;
+  private void login;
+  private void logout;
+  
+  //
+  // Constructors
+  //
+  public internet () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of enter_id
+   * @param newVar the new value of enter_id
+   */
+  private void setEnter_id (void newVar) {
+    enter_id = newVar;
+  }
+
+  /**
+   * Get the value of enter_id
+   * @return the value of enter_id
+   */
+  private void getEnter_id () {
+    return enter_id;
+  }
+
+  /**
+   * Set the value of download
+   * @param newVar the new value of download
+   */
+  private void setDownload (void newVar) {
+    download = newVar;
+  }
+
+  /**
+   * Get the value of download
+   * @return the value of download
+   */
+  private void getDownload () {
+    return download;
+  }
+
+  /**
+   * Set the value of login
+   * @param newVar the new value of login
+   */
+  private void setLogin (void newVar) {
+    login = newVar;
+  }
+
+  /**
+   * Get the value of login
+   * @return the value of login
+   */
+  private void getLogin () {
+    return login;
+  }
+
+  /**
+   * Set the value of logout
+   * @param newVar the new value of logout
+   */
+  private void setLogout (void newVar) {
+    logout = newVar;
+  }
+
+  /**
+   * Get the value of logout
+   * @return the value of logout
+   */
+  private void getLogout () {
+    return logout;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void verify()
+  {
+  }
+
+
+  /**
+   */
+  public void status()
+  {
+  }
+
+
+}
