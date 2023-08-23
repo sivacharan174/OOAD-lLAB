5772f6aa67282fa4778fa3b0951a071043687580
sivacharan174 <129138943+sivacharan174@users.noreply.github.com>
2023-08-23 08:21:49 +0530

    Add files via upload

---
+++ EXP6/railway_reservation_system1.java
@@ -0,0 +1,110 @@
+
+
+/**
+ * Class railway_reservation_system1
+ */
+public class railway_reservation_system1 {
+
+  //
+  // Fields
+  //
+
+  private void train_number;
+  private void train_name;
+  private void passenger_name;
+  
+  //
+  // Constructors
+  //
+  public railway_reservation_system1 () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of train_number
+   * @param newVar the new value of train_number
+   */
+  private void setTrain_number (void newVar) {
+    train_number = newVar;
+  }
+
+  /**
+   * Get the value of train_number
+   * @return the value of train_number
+   */
+  private void getTrain_number () {
+    return train_number;
+  }
+
+  /**
+   * Set the value of train_name
+   * @param newVar the new value of train_name
+   */
+  private void setTrain_name (void newVar) {
+    train_name = newVar;
+  }
+
+  /**
+   * Get the value of train_name
+   * @return the value of train_name
+   */
+  private void getTrain_name () {
+    return train_name;
+  }
+
+  /**
+   * Set the value of passenger_name
+   * @param newVar the new value of passenger_name
+   */
+  private void setPassenger_name (void newVar) {
+    passenger_name = newVar;
+  }
+
+  /**
+   * Get the value of passenger_name
+   * @return the value of passenger_name
+   */
+  private void getPassenger_name () {
+    return passenger_name;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void status()
+  {
+  }
+
+
+  /**
+   */
+  public void reservation()
+  {
+  }
+
+
+  /**
+   */
+  public void cancellation()
+  {
+  }
+
+
+  /**
+   */
+  public void railway_reservation_system1()
+  {
+  }
+
+
+}
