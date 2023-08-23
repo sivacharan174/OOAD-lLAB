3402152c30a1f7ca668c8361b2f6a23fca7bfc0d
sivacharan174 <129138943+sivacharan174@users.noreply.github.com>
2023-08-23 09:56:27 +0530

    Add files via upload

---
+++ EXP29/Buses.java
@@ -0,0 +1,96 @@
+
+
+/**
+ * Class Buses
+ */
+public class Buses {
+
+  //
+  // Fields
+  //
+
+  private void Bus_no;
+  private void capacity;
+  private void model;
+  
+  //
+  // Constructors
+  //
+  public Buses () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of Bus_no
+   * @param newVar the new value of Bus_no
+   */
+  private void setBus_no (void newVar) {
+    Bus_no = newVar;
+  }
+
+  /**
+   * Get the value of Bus_no
+   * @return the value of Bus_no
+   */
+  private void getBus_no () {
+    return Bus_no;
+  }
+
+  /**
+   * Set the value of capacity
+   * @param newVar the new value of capacity
+   */
+  private void setCapacity (void newVar) {
+    capacity = newVar;
+  }
+
+  /**
+   * Get the value of capacity
+   * @return the value of capacity
+   */
+  private void getCapacity () {
+    return capacity;
+  }
+
+  /**
+   * Set the value of model
+   * @param newVar the new value of model
+   */
+  private void setModel (void newVar) {
+    model = newVar;
+  }
+
+  /**
+   * Get the value of model
+   * @return the value of model
+   */
+  private void getModel () {
+    return model;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void time_of_depature()
+  {
+  }
+
+
+  /**
+   */
+  public void time_of_arrival()
+  {
+  }
+
+
+}
