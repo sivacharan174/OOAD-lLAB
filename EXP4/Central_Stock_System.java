3d5ef26ebab81dae90a93ee38fc1dd63c752250b
sivacharan174 <129138943+sivacharan174@users.noreply.github.com>
2023-08-23 08:15:02 +0530

    Add files via upload

---
+++ EXP4/Central_Stock_System.java
@@ -0,0 +1,62 @@
+
+
+/**
+ * Class Central_Stock_System
+ */
+public class Central_Stock_System {
+
+  //
+  // Fields
+  //
+
+  private void store_stock_details;
+  
+  //
+  // Constructors
+  //
+  public Central_Stock_System () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of store_stock_details
+   * @param newVar the new value of store_stock_details
+   */
+  private void setStore_stock_details (void newVar) {
+    store_stock_details = newVar;
+  }
+
+  /**
+   * Get the value of store_stock_details
+   * @return the value of store_stock_details
+   */
+  private void getStore_stock_details () {
+    return store_stock_details;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void printbill()
+  {
+  }
+
+
+  /**
+   */
+  public void delivery_product()
+  {
+  }
+
+
+}
---
+++ EXP4/customer.java
@@ -0,0 +1,55 @@
+
+
+/**
+ * Class customer
+ */
+public class customer {
+
+  //
+  // Fields
+  //
+
+  private void place_order;
+  
+  //
+  // Constructors
+  //
+  public customer () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of place_order
+   * @param newVar the new value of place_order
+   */
+  private void setPlace_order (void newVar) {
+    place_order = newVar;
+  }
+
+  /**
+   * Get the value of place_order
+   * @return the value of place_order
+   */
+  private void getPlace_order () {
+    return place_order;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void payment()
+  {
+  }
+
+
+}
