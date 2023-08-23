d35883cb4d4dc169aa5e855796530bb15d1e9225
sivacharan174 <129138943+sivacharan174@users.noreply.github.com>
2023-08-23 08:13:28 +0530

    Add files via upload

---
+++ EXP3/Students.java
@@ -0,0 +1,130 @@
+
+
+/**
+ * Class Students
+ */
+public class Students {
+
+  //
+  // Fields
+  //
+
+  private void Name;
+  private void Address;
+  private void DOB;
+  private void Subjects;
+  private void College;
+  
+  //
+  // Constructors
+  //
+  public Students () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of Name
+   * @param newVar the new value of Name
+   */
+  private void setName (void newVar) {
+    Name = newVar;
+  }
+
+  /**
+   * Get the value of Name
+   * @return the value of Name
+   */
+  private void getName () {
+    return Name;
+  }
+
+  /**
+   * Set the value of Address
+   * @param newVar the new value of Address
+   */
+  private void setAddress (void newVar) {
+    Address = newVar;
+  }
+
+  /**
+   * Get the value of Address
+   * @return the value of Address
+   */
+  private void getAddress () {
+    return Address;
+  }
+
+  /**
+   * Set the value of DOB
+   * @param newVar the new value of DOB
+   */
+  private void setDOB (void newVar) {
+    DOB = newVar;
+  }
+
+  /**
+   * Get the value of DOB
+   * @return the value of DOB
+   */
+  private void getDOB () {
+    return DOB;
+  }
+
+  /**
+   * Set the value of Subjects
+   * @param newVar the new value of Subjects
+   */
+  private void setSubjects (void newVar) {
+    Subjects = newVar;
+  }
+
+  /**
+   * Get the value of Subjects
+   * @return the value of Subjects
+   */
+  private void getSubjects () {
+    return Subjects;
+  }
+
+  /**
+   * Set the value of College
+   * @param newVar the new value of College
+   */
+  private void setCollege (void newVar) {
+    College = newVar;
+  }
+
+  /**
+   * Get the value of College
+   * @return the value of College
+   */
+  private void getCollege () {
+    return College;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void Form_Filling()
+  {
+  }
+
+
+  /**
+   */
+  public void Registration()
+  {
+  }
+
+
+}
---
+++ EXP3/University_Databases.java
@@ -0,0 +1,72 @@
+
+
+/**
+ * Class University_Databases
+ */
+public class University_Databases {
+
+  //
+  // Fields
+  //
+
+  private void Exam_Details;
+  private void Reg_Details;
+  
+  //
+  // Constructors
+  //
+  public University_Databases () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of Exam_Details
+   * @param newVar the new value of Exam_Details
+   */
+  private void setExam_Details (void newVar) {
+    Exam_Details = newVar;
+  }
+
+  /**
+   * Get the value of Exam_Details
+   * @return the value of Exam_Details
+   */
+  private void getExam_Details () {
+    return Exam_Details;
+  }
+
+  /**
+   * Set the value of Reg_Details
+   * @param newVar the new value of Reg_Details
+   */
+  private void setReg_Details (void newVar) {
+    Reg_Details = newVar;
+  }
+
+  /**
+   * Get the value of Reg_Details
+   * @return the value of Reg_Details
+   */
+  private void getReg_Details () {
+    return Reg_Details;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void Storing_registration()
+  {
+  }
+
+
+}
