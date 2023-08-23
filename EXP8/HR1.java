23ce72ed7f094b32fdad9d80d1f94f70d04b5539
sivacharan174 <129138943+sivacharan174@users.noreply.github.com>
2023-08-23 08:26:21 +0530

    Add files via upload

---
+++ EXP8/HR1.java
@@ -0,0 +1,69 @@
+
+
+/**
+ * Class HR1
+ */
+public class HR1 {
+
+  //
+  // Fields
+  //
+
+  private void check_details;
+  
+  //
+  // Constructors
+  //
+  public HR1 () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of check_details
+   * @param newVar the new value of check_details
+   */
+  private void setCheck_details (void newVar) {
+    check_details = newVar;
+  }
+
+  /**
+   * Get the value of check_details
+   * @return the value of check_details
+   */
+  private void getCheck_details () {
+    return check_details;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void loss_of_pay()
+  {
+  }
+
+
+  /**
+   */
+  public void tax()
+  {
+  }
+
+
+  /**
+   */
+  public void HR1()
+  {
+  }
+
+
+}
