906ddc24b00d953ae439dc64a9846dfe33565e0a
sivacharan174 <129138943+sivacharan174@users.noreply.github.com>
2023-08-23 10:12:56 +0530

    Add files via upload

---
+++ EXP31/admin (2).java
@@ -0,0 +1,113 @@
+
+
+/**
+ * Class admin
+ */
+public class admin {
+
+  //
+  // Fields
+  //
+
+  private void adminid;
+  private void user_name;
+  private void password;
+  private void add_customer;
+  
+  //
+  // Constructors
+  //
+  public admin () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of adminid
+   * @param newVar the new value of adminid
+   */
+  private void setAdminid (void newVar) {
+    adminid = newVar;
+  }
+
+  /**
+   * Get the value of adminid
+   * @return the value of adminid
+   */
+  private void getAdminid () {
+    return adminid;
+  }
+
+  /**
+   * Set the value of user_name
+   * @param newVar the new value of user_name
+   */
+  private void setUser_name (void newVar) {
+    user_name = newVar;
+  }
+
+  /**
+   * Get the value of user_name
+   * @return the value of user_name
+   */
+  private void getUser_name () {
+    return user_name;
+  }
+
+  /**
+   * Set the value of password
+   * @param newVar the new value of password
+   */
+  private void setPassword (void newVar) {
+    password = newVar;
+  }
+
+  /**
+   * Get the value of password
+   * @return the value of password
+   */
+  private void getPassword () {
+    return password;
+  }
+
+  /**
+   * Set the value of add_customer
+   * @param newVar the new value of add_customer
+   */
+  private void setAdd_customer (void newVar) {
+    add_customer = newVar;
+  }
+
+  /**
+   * Get the value of add_customer
+   * @return the value of add_customer
+   */
+  private void getAdd_customer () {
+    return add_customer;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void view_customer()
+  {
+  }
+
+
+  /**
+   */
+  public void check_defalt()
+  {
+  }
+
+
+}
