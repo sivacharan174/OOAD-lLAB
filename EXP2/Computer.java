706989c8968478e44c972397653d4cc10debcc1a
sivacharan174 <129138943+sivacharan174@users.noreply.github.com>
2023-08-23 08:11:12 +0530

    Add files via upload

---
+++ EXP2/Computer.java
@@ -0,0 +1,93 @@
+
+
+/**
+ * Class Computer
+ */
+public class Computer {
+
+  //
+  // Fields
+  //
+
+  private void student_record;
+  private void booklist;
+  
+  //
+  // Constructors
+  //
+  public Computer () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of student_record
+   * @param newVar the new value of student_record
+   */
+  private void setStudent_record (void newVar) {
+    student_record = newVar;
+  }
+
+  /**
+   * Get the value of student_record
+   * @return the value of student_record
+   */
+  private void getStudent_record () {
+    return student_record;
+  }
+
+  /**
+   * Set the value of booklist
+   * @param newVar the new value of booklist
+   */
+  private void setBooklist (void newVar) {
+    booklist = newVar;
+  }
+
+  /**
+   * Get the value of booklist
+   * @return the value of booklist
+   */
+  private void getBooklist () {
+    return booklist;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void maintainstudrecord()
+  {
+  }
+
+
+  /**
+   */
+  public void enter_issue()
+  {
+  }
+
+
+  /**
+   */
+  public void order_new_autor()
+  {
+  }
+
+
+  /**
+   */
+  public void check_availability()
+  {
+  }
+
+
+}
---
+++ EXP2/stud.java
@@ -0,0 +1,69 @@
+
+
+/**
+ * Class stud
+ */
+public class stud {
+
+  //
+  // Fields
+  //
+
+  private void stud_details;
+  
+  //
+  // Constructors
+  //
+  public stud () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of stud_details
+   * @param newVar the new value of stud_details
+   */
+  private void setStud_details (void newVar) {
+    stud_details = newVar;
+  }
+
+  /**
+   * Get the value of stud_details
+   * @return the value of stud_details
+   */
+  private void getStud_details () {
+    return stud_details;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void request_for_books()
+  {
+  }
+
+
+  /**
+   */
+  public void return_previous_books()
+  {
+  }
+
+
+  /**
+   */
+  public void register()
+  {
+  }
+
+
+}
